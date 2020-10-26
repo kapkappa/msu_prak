03f6204b77ac1ccc9dbf0fdaf319bd8001baae3b
romankupry <roman.kupry@gmail.com>
2020-10-26 18:07:09 +0300

    Utf8 to UCS converter, works fine with correct files, but too strict with incorrect files (few bytes are not written, in comparison to test files

--- prac2/utf2ucs.c
+++ prac2/utf2ucs.c
@@ -1,60 +1,151 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
-/*
-	Read from stdin, write in stdout
-	I want to read some bytes in UTF-8, this means:
-	there are 3 types of encoded symbols - 1, 2 and 3 bytes for symbol
+#define FIRST_BIT 0x80
+#define TWO_BITS 0xC0
+#define THREE_BITS 0xE0
+#define FOUR_BITS 0xF0
 
-	0[7] - 1 byte for syvbol,
-	110[5] AND 10[6] - 2 bytes for symbol,
-	1110[4] AND 10[6] AND 10[6] - 3 bytes for symbol,
+void read_utf_bom(FILE *fin)
+{
+	unsigned char b1, b2, b3;
+	fread(&b1, 1, 1, fin);
+	fread(&b2, 1, 1, fin);
+	fread(&b3, 1, 1, fin);
+	if(!((b1==0xEF) && (b2==0xBB) && (b3==0xBF)))
+	{
+		fseek(fin, 0, SEEK_SET);
+	}
+}
 
-	So we need to check first bits in read byte. And then decide how it will be decoded into UTF-16
+int read_ucs_bom(char*filename)
+{
+	unsigned short bom = 0;
+	int order;
+	FILE *fout = fopen(filename, "r");
+	if(fout)
+	{
+		fread(&bom, 2, 1, fout);
+		fclose(fout);
+	}
+	if(bom == 0xFEFF) order = 1;			// Becouse of reverse reading, in file is FFFE, thats mean it is LE order.
+	else if(bom == 0xFFFE) order = 0;		// Same thing here, in file there are FEFF bytes, which means its BE order.
+	else order = 2; 				// No BOM, defoult is LE
+	fout = fopen(filename, "w");
+	if(!fout){perror("Can't rewrite output file"); exit(1);}
+	if(order != 2) fwrite(&bom, 2, 1, fout);	// Write is reversing too, so bom doesnt changes.
+	fclose(fout);
+	return order;
+}
 
-	In UCS one symbol is encoded with 2 bytes OR with 2 pairs of bytes
-*/
-
-#define FIRST_BIT 0200
+//TODO optimize LE/BE move out from cycle
 
 int main(int argc, char**argv)
 {
-
-//in case that we need to read symbols byte after byte then we can use any read function
 	FILE *fin, *fout;
 	fin = stdin;
 	fout = stdout;
-	char LE = 1; // We have preordered numeration - LESS ORDER!
-	char first, second, third, fbit;
-	unsigned short ucs_byte;
-	fread(&first, sizeof(char), 1, fin);
-	fread(&second, sizeof(char), 1, fin);
-	if((first == 0xFF) && (second == 0xFE))
+	int byte_order = 2;				 // LE is set defoult
+	if(argc==2)
 	{
-		LE = 1;
+		fin = fopen(argv[1], "r");
+		if(!fin){perror("Can't read from the input file"); exit(1);}
 	}
-	else if((first == 0xFE) && (second == 0xFF))
+	else if(argc>2)
 	{
-		LE = 0; // WE HAVE BIG ENDIAN
+		fin = fopen(argv[1], "r");
+		if(!fin){perror("Can't read from the input file"); exit(1);}
+		byte_order = read_ucs_bom(argv[2]);
+		fout = fopen(argv[2], "a");
+		if(!fout){perror("Can't open or create output file"); exit(1);}
 	}
-	else
+	read_utf_bom(fin);
+	unsigned char first, second, third;
+	unsigned char part1, part2;
+
+	if(!byte_order)					// BE is 0, otherwise its LE.
 	{
-		LE = 1;
-		ungetc(second, fin);
-		ungetc(first, fin);
 	}
 
-	while(!feof(fin))
+	while(fread(&first, 1, 1, fin))
 	{
-		fread(&first, sizeof(char), 1, fin);
-		// Проверить первые биты
-		fbit = first & FIRST_BIT;
-		if(!fbit)
+		part2 = part1 = 0;
+		if(!(first & FIRST_BIT))		// 1 byte 1 symbol
 		{
-			// 1 BYTE 1 SYMBOL
-			ucs_byte = 0;
-			ucs_byte = ucs_byte | first;
-			fwrite(&ucs_byte, sizeof(ucs_byte), 1, fout);
+			part1 = first;
+			if(!byte_order)
+			{
+				part2 = part1;
+				part1 = 0;
+			}
 		}
+		else if((first & THREE_BITS) == 0xC0)	// first byte from two bytes
+		{
+			first &= 0x1F;
+			fread(&second, 1, 1, fin);
+			if((second & TWO_BITS) != 0x80)	//second byte is incorrect!
+			{
+				fprintf(stderr, "In 2-bytes sequence, the second byte is incorrect!\n");
+				fprintf(stderr, "Incorrect byte is %x. And it's position is %ld\n", second, ftell(fin));
+				continue;
+			}
+			second &= 0x3F;
+
+			part1 = first & 0x3;
+			part1 <<=6;
+			part1 += second;
+			part2 = first >> 2;
+
+			if(!byte_order)
+			{
+				unsigned char tmp = part1;
+				part1 = part2;
+				part2 = tmp;
+			}
+		}
+		else if((first & FOUR_BITS) == 0xE0) // first byte from three bytes
+		{
+			first &= 0xF;
+			fread(&second, 1, 1, fin);
+			if((second & TWO_BITS) != 0x80)
+			{
+				fprintf(stderr, "In 3-bytes sequence, the second byte is incorrect!\n");
+				fprintf(stderr, "Incorrect byte is %x. And it's position is %ld\n", second, ftell(fin));
+				continue;
+			}
+			second &= 0x3F;
+			fread(&third, 1, 1, fin);
+			if((third & TWO_BITS) != 0x80)
+			{
+				fprintf(stderr, "In 3-bytes sequence, the third byte is incorrect!\n");
+				fprintf(stderr, "Incorrect byte is %x. And it's position is %ld\n", third, ftell(fin));
+				continue;
+			}
+			third &= 0x3F;
+
+			part1 = second & 0x3;
+			part1 <<= 6;
+			part1 += third;
+			part2 = second>>2;
+			part2 += first << 4;
+
+			if(!byte_order)
+			{
+				unsigned short tmp = part1;
+				part1 = part2;
+				part2 = tmp;
+			}
+		}
+		else
+		{
+			fprintf(stderr, "The first byte is incorrect\n");
+			fprintf(stderr, "Incorrect byte is %x. And it's position is %ld\n", first, ftell(fin));
+			continue;
+		}
+		fwrite(&part1, 1, 1, fout);
+		fwrite(&part2, 1, 1, fout);
 	}
+	fclose(fin);
+	fclose(fout);
 }
