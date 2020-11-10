031c125fde3ad1917e42408171604de4a42c4f62
romankupry <roman.kupry@gmail.com>
2020-11-10 17:25:06 +0300

    Optimizing reading function, removing 'if-else' from the loop

--- prac2/ucs2utf.c
+++ prac2/ucs2utf.c
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include <errno.h>
 
-/*
-	We have 2 byte_orders - LE and BE, which defined in input file, by BOM. If not then LE - is set as defoult.
-	Next, we have the whole byte, and we can check the boundares, which tell us, how many bytes, we need to use - 1, 2 or 3 (first, f+second, f+s+third)
-*/
-
-
 int read_ucs_bom(FILE *fin)
 {
 	unsigned short bom;
@@ -36,7 +30,11 @@ void read_utf_bom(char *filename)
 		fclose(fout);
 	}
 	fout = fopen(filename, "w");
-	if(!fout){perror("Can't rewrite the output file."); exit(1);}
+	if(!fout)
+	{
+		perror("Can't rewrite the output file.");
+		exit(1);
+	}
 	if((b1 == 0xEF) && (b2 == 0xBB) && (b3 == 0xBF))
 	{
 		fwrite(&b1, 1, 1, fout);
@@ -46,54 +44,74 @@ void read_utf_bom(char *filename)
 	fclose(fout);
 }
 
+long BE_reading(unsigned char*part1, unsigned char*part2, FILE*fin)
+{
+	long counter = fread(part2, 1, 1, fin);
+	counter += fread(part1, 1, 1, fin);
+	return counter;
+}
+
+long LE_reading(unsigned char*part1, unsigned char*part2, FILE*fin)
+{
+	long counter = fread(part1, 1, 1, fin);
+	counter += fread(part2, 1, 1, fin);
+	return counter;
+}
 
 int main(int argc, char** argv)
 {
 	FILE *fin, *fout;
 	fin = stdin;
 	fout = stdout;
-	int byte_order = 2; //LE - is set default.
+	int byte_order = 2;							//LE - is set default.
 	if(argc > 1)
 	{
 		fin = fopen(argv[1], "r");
-		if(!fin){perror("Can't read form input file"); exit(1);}
+		if(!fin)
+		{
+			perror("Can't read form input file");
+			exit(1);
+		}
 		byte_order = read_ucs_bom(fin);
-
 		if(argc > 2)
 		{
 			read_utf_bom(argv[2]);
 			fout = fopen(argv[2], "a");
-			if(!fout){perror("Can't write into the output file"); exit(1);}
+			if(!fout)
+			{
+				perror("Can't write into the output file");
+				exit(1);
+			}
 		}
 	}
-	fprintf(stderr, "byte_order is %d\n", byte_order);
+
 	unsigned char part1, part2, first, second, third;
 	unsigned short symbol;
 	long counter = 0;
+
+	long (*reading) (unsigned char*, unsigned char*, FILE*);
+	reading = LE_reading;
+	if(!byte_order)
+	{
+		reading = BE_reading;
+	}
+
 	while(!feof(fin))
 	{
-		counter = fread(&part1, 1, 1, fin);
-		counter += fread(&part2, 1, 1, fin);
+		counter = reading(&part1, &part2, fin);
 		if(counter != 2)
 		{
 			if(counter == 1) fprintf(stderr, "ATTENTION, INCORRECT BYTE'S LENGHT!\n");
 			continue;
 		}
-		if(!byte_order)
-		{
-			unsigned char tmp = part1;
-			part1 = part2;
-			part2 = tmp;
-		}
 		symbol = part2;
 		symbol <<= 8;
 		symbol += part1;
-
-		if(symbol < 0x80)			//We need 1 byte in UTF-8
+		if(symbol < 0x80)					//We need 1 byte in UTF-8
 		{
 			fwrite(&part1, 1, 1, fout);
 		}
-		else if(symbol < 0x800)			//We need 2 bytes: 110[7] + 10[6].
+		else if(symbol < 0x800)					//We need 2 bytes: 110[7] + 10[6].
 		{
 			first = part2 & 0x7;
 			first <<= 2;
@@ -103,7 +121,7 @@ int main(int argc, char** argv)
 			fwrite(&first, 1, 1, fout);
 			fwrite(&second, 1, 1, fout);
 		}
-		else if((symbol < 0xD800) || (symbol > 0xDFFF))	//We need 3 bytes: 1110[4] + 10[6] + 10[6]
+		else if((symbol < 0xD800) || (symbol > 0xDFFF))		//We need 3 bytes: 1110[4] + 10[6] + 10[6]
 		{
 			third = 0x80 + (part1 & 0x3F);
 			second = part2 & 0xF;
@@ -122,7 +140,6 @@ int main(int argc, char** argv)
 			fprintf(stderr, "Incorrect symbol\n");
 		}
 	}
-
 	fclose(fin);
 	fclose(fout);
 	return 0;
