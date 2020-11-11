8ccd8d678b0c935d492e3642740d2fac2f604961
romankupry <roman.kupry@gmail.com>
2020-11-11 13:58:09 +0300

    3/5 jf 6's homework and minor correcting README from 5's HW

---
+++ 6_hometask/ht_6_2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char**argv)
+{
+	if(argc!=3)
+	{
+		fprintf(stderr, "Need operations names\n");
+		return 1;
+	}
+	pid_t p;
+	int status=0;
+	if((p = fork()) == 0)
+	{
+		// SON
+		execlp(argv[1], argv[1], NULL);
+		perror("First operation's fault");
+		exit(1);
+	}
+	wait(&status);
+	if(!(WIFEXITED(status) && WEXITSTATUS(status)==0))
+	{
+		return 0;
+	}
+	if((p = fork()) == 0)
+	{
+		execlp(argv[2], argv[2], NULL);
+		perror("Second operation's fault");
+		exit(1);
+	}
+	wait(&status);
+	return 0;
+}
