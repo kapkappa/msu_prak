fab81eb07ce73849d8c4a739f26840586fd0f5b2
roman <roman.kupry@gmail.com>
2022-09-28 14:34:54 +0300

    QC started

---
+++ sem7/task1/main.cpp
@@ -0,0 +1,16 @@
+#include <complex>
+
+int main(int argc, char** argv) {
+
+    int number_of_atoms = 2;
+    bool interaction = true;
+    double frequency_atom = 0.0;
+    double frequency_photon = 0.0;
+    int min_number_of_photons = 0;
+    int max_number_of_photons = 2;
+    double d_time = 0.001;
+    double HPLANK = 6.62606957e-27;
+    int steps = 10;
+
+    return 0;
+}
